edf0f077c80ad4860db854b3459f7985c043dfa0
Filipe <filipe.torqueto@gmail.com>
2019-10-16 09:56:22 -0300

    Creation f the project Close #1

---
+++ src/test/java/com/fitsoftware/foodapi/FoodapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fitsoftware.foodapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodapiApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
